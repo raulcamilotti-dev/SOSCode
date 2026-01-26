c4ca9ebdbde97b5e813308d22308a02361cfa05c
raulcamilotti-dev <raulcamilotti@gmail.com>
2026-01-26 12:55:35 -0300

    v1.0

---
+++ app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import authRoutes from "./routes/login";
+import db from "./db";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+app.get("/health/db", async (_req, res) => {
+  await db.query("SELECT 1");
+  res.json({ ok: true });
+});
+
+export default app;
